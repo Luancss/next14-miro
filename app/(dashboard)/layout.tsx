064783f23e538fe3b228b2966b6f4cd52d2b9d1d
Luancss <ventobo123@gmail.com>
2024-02-12 18:24:10 -0300

    Create organization sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,4 @@
+import { OrgSidebar } from "./_components/org-sidebar"
 import { Sidebar } from "./_components/sidebar"
 
 interface DashboardLayoutProps {
@@ -12,6 +13,7 @@ const DashboardLayout = ({
       <Sidebar/>
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
+          <OrgSidebar/>
         <div className="h-full flex-1">
         {children}
         </div>
