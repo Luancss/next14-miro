7595f570d0ad6cdc86251e275bbea8f1cd04e0fc
Luancss <ventobo123@gmail.com>
2024-02-12 18:35:41 -0300

    Navbar setup and searchbar setup

---
+++ app/(dashboard)/_components/navbar.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import { UserButton } from "@clerk/nextjs";
+
+export const Navbar = () => {
+  return (
+    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+      <div className="hidden lg:flex lg:flex-1">
+        Search
+
+        </div>
+      <UserButton />
+    </div>
+  );
+};
--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,26 +1,26 @@
-import { OrgSidebar } from "./_components/org-sidebar"
-import { Sidebar } from "./_components/sidebar"
+import { Navbar } from "./_components/navbar";
+import { OrgSidebar } from "./_components/org-sidebar";
+import { Sidebar } from "./_components/sidebar";
 
 interface DashboardLayoutProps {
-  children: React.ReactNode
+  children: React.ReactNode;
 }
 
-const DashboardLayout = ({
-  children,
-}: DashboardLayoutProps) => {
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
-    <main className="w-full">
-      <Sidebar/>
+    <main className="h-full">
+      <Sidebar />
       <div className="pl-[60px] h-full">
         <div className="flex gap-x-3 h-full">
-          <OrgSidebar/>
-        <div className="h-full flex-1">
-        {children}
-        </div>
+          <OrgSidebar />
+          <div className="h-full flex-1">
+            <Navbar />
+            {children}
+          </div>
         </div>
       </div>
     </main>
-  )
-}
+  );
+};
 
-export default DashboardLayout
+export default DashboardLayout;
