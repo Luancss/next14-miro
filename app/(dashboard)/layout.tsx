914a330dc4a021cc1cc3c711a866977e4d58e5b0
Luancss <ventobo123@gmail.com>
2024-02-12 18:11:52 -0300

    Layout on page app

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,15 @@
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+const DashboardLayout = ({
+  children,
+}: DashboardLayoutProps) => {
+  return (
+    <div>
+      {children}
+    </div>
+  )
+}
+
+export default DashboardLayout
