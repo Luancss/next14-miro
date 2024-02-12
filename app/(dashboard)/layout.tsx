6e8f56f25a78c70512e95477bd53102c90ccf166
Luancss <ventobo123@gmail.com>
2024-02-12 18:20:12 -0300

    Layout sidebar

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,3 +1,5 @@
+import { Sidebar } from "./_components/sidebar"
+
 interface DashboardLayoutProps {
   children: React.ReactNode
 }
@@ -6,9 +8,16 @@ const DashboardLayout = ({
   children,
 }: DashboardLayoutProps) => {
   return (
-    <div>
-      {children}
-    </div>
+    <main className="w-full">
+      <Sidebar/>
+      <div className="pl-[60px] h-full">
+        <div className="flex gap-x-3 h-full">
+        <div className="h-full flex-1">
+        {children}
+        </div>
+        </div>
+      </div>
+    </main>
   )
 }
 
