b83103a6923bbcd2d8b4866cd5beeb626e9d3aa0
Luancss <ventobo123@gmail.com>
2024-02-14 16:55:43 -0300

    Organization Switcher Trigger

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -39,6 +39,14 @@ export const OrgSidebar = () => {
             justifyContent: "center",
             alignItems: "center",
             width: "100%",
+          },
+          organizationSwitcherTrigger: {
+            padding: "6px",
+            width: "100%",
+            borderRadius: "8px",
+            border: "1px solid #E5E7EB",
+            justifyContent: "space-between",
+            backgroundColor: "white",
           }
          }
         }}
