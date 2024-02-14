5014027ce8bf5ef2da77302cc0f190a33f075453
Luancss <ventobo123@gmail.com>
2024-02-14 16:44:40 -0300

    Organization sidebar

--- app/(dashboard)/_components/org-sidebar.tsx
+++ app/(dashboard)/_components/org-sidebar.tsx
@@ -1,9 +1,39 @@
 "use client"
 
+import Link from "next/link";
+import Image from "next/image";
+import { Poppins } from "next/font/google";
+import { cn } from "@/lib/utils";
+import { OrganizationSwitcher } from "@clerk/nextjs";
+
+const font = Poppins({
+  subsets: ["latin"],
+  weight: "600",
+})
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden lg:flex flex-col space-x-6 w-[206px] pl-5 pt-5 bg-red-500">
-      Org Sidebar
+    <div className="hidden lg:flex flex-col space-x-6 w-[206px] pl-5 pt-5">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image
+            src="/logo.png"
+            alt="Logo"
+            width={60}
+            height={60}
+          />
+        <span className={cn(
+          "text-2xl font-semibold",
+          font.className
+        )}>
+          Board
+        </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        
+      />
+      
     </div>
   )
 }
--- app/(dashboard)/_components/sidebar/item.tsx
+++ app/(dashboard)/_components/sidebar/item.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import { useOrganization, useOrganizationList } from "@clerk/nextjs";
 
 import { cn } from "@/lib/utils";
+import { Hint } from "@/components/hint";
 
 interface ItemProps {
   id: string;
@@ -25,6 +26,12 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
 
   return (
     <div className="aspect-square relative">
+      <Hint 
+        label={name}
+        side="right"
+        align="start"
+        sideOffset={18}
+      >
       <Image
         fill
         src={imageUrl}
@@ -34,6 +41,7 @@ export const Item = ({ id, name, imageUrl }: ItemProps) => {
           "rounded-md cursor-pointer opacity-75 hover:opacity-100 transition", isActive && "opacity-100"
         )}
       />
+      </Hint>
     </div>
   );
 };
