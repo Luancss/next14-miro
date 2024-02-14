9053c70e202b3cbfbfb8df126b179979c715f94f
Luancss <ventobo123@gmail.com>
2024-02-14 00:59:10 -0300

    adjusting background error

--- app/(dashboard)/_components/sidebar/new-button.tsx
+++ app/(dashboard)/_components/sidebar/new-button.tsx
@@ -15,7 +15,7 @@ export const NewButton = () => {
           </button>
         </div>
       </DialogTrigger>
-      <DialogContent>
+      <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
         <CreateOrganization />
       </DialogContent>
     </Dialog>
