a7ad0c95a826ea8735fb7b52b1e07114d2c87d08
Luancss <ventobo123@gmail.com>
2024-02-13 21:51:16 -0300

    install shacn-ui dialog and tooltip

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -7,7 +7,6 @@ export const Navbar = () => {
     <div className="flex items-center gap-x-4 p-5 bg-green-500">
       <div className="hidden lg:flex lg:flex-1">
         Search
-
         </div>
       <UserButton />
     </div>
