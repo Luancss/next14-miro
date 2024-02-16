16e13fcef7f4a3603a7137769fad9b9122e75abd
Luancss <ventobo123@gmail.com>
2024-02-15 23:43:51 -0300

    Create a navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import { UserButton } from "@clerk/nextjs";
+import { SeatchInput } from "./search-input";
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 p-5 bg-green-500">
+    <div className="flex items-center gap-x-4 p-">
       <div className="hidden lg:flex lg:flex-1">
-        Search
+        <SeatchInput/>
         </div>
       <UserButton />
     </div>
---
+++ app/(dashboard)/_components/search-input.tsx
@@ -0,0 +1,15 @@
+"use client"
+
+import qs from "query-string"
+import { Search } from "lucide-react"
+import { useDebounce } from "usehooks-ts"
+import { useRouter } from "next/navigation"
+import { ChangeEvent, useState, useEffect } from "react"
+
+export const SeatchInput = () => {
+  return (
+    <div>
+      Search
+    </div>
+  )
+}
