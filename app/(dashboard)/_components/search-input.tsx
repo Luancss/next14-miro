c6450053f51d7a5081f97c16ac32db8ebcf6645e
Luancss <ventobo123@gmail.com>
2024-02-15 23:52:58 -0300

    search input with dynamic route

--- app/(dashboard)/_components/search-input.tsx
+++ app/(dashboard)/_components/search-input.tsx
@@ -5,11 +5,41 @@ import { Search } from "lucide-react"
 import { useDebounce } from "usehooks-ts"
 import { useRouter } from "next/navigation"
 import { ChangeEvent, useState, useEffect } from "react"
+import { Input } from "@/components/ui/input"
 
 export const SeatchInput = () => {
+  const router = useRouter();
+  const [value, setValue] = useState("");
+  const denbouncedValue = useDebounce(value, 500);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  }
+
+  useEffect(() => {
+    const url = qs.stringifyUrl({
+      url: "/",
+      query: {
+        search: denbouncedValue
+      }
+    }, {
+      skipEmptyString: true, skipNull: true
+    })
+
+    router.push(url)
+  }, [denbouncedValue, router])
+
   return (
-    <div>
-      Search
+    <div className="w-full relative">
+      <Search
+        className="absolute top-1/2 left-3 transform -translate-y-1/2 text-muted-foreground h-4 w-4"
+      />
+      <Input 
+        className="w-full max-w-[516px] pl-9"
+        placeholder="Search boards"
+        onChange={handleChange}
+        value={value}
+      />
     </div>
   )
 }
