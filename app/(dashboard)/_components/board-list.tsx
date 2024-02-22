07efbe76489f5f4b4bf7452e6d13a6b224311280
Luancss <ventobo123@gmail.com>
2024-02-21 23:28:45 -0300

    Conditionals on the boardlist

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -9,6 +9,32 @@ interface BoardListProps {
 };
 
 export const BoardList = ({ orgId, query }: BoardListProps) => {
+  const data = [];
+
+  if (!data?.length && query.search) {
+    return (
+      <div>
+        Try searching for something else
+        </div>
+    )
+  }
+
+  if (!data?.length && query.favorites) {
+    return (
+      <div>
+        No favorites
+      </div>
+    )
+  }
+
+  if (!data?.length) {
+    return (
+      <div>
+        No board at all
+      </div>
+    )
+  }
+
   return (
     <div>
       {JSON.stringify(query)}
--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,6 +1,7 @@
 "use client"
 import { useOrganization } from "@clerk/nextjs";
 import { EmptyOrg } from "./_components/empty-org";
+import { BoardList } from "./_components/board-list";
 
 interface DashboardLayoutProps {
   searchParams: {
