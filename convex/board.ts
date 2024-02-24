00b9854879a127b24429acc6b1e870bbe93d43f9
Luancss <ventobo123@gmail.com>
2024-02-23 23:47:41 -0300

    Create a board mutation

---
+++ convex/board.ts
@@ -0,0 +1,29 @@
+import { mutation } from "./_generated/server"
+import {v} from "convex/values";
+
+const images = [
+  "/placeholder/1.svg",
+  "/placeholder/2.svg",
+  "/placeholder/3.svg",
+  "/placeholder/4.svg",
+  "/placeholder/5.svg",
+  "/placeholder/6.svg",
+  "/placeholder/7.svg",
+  "/placeholder/8vg",
+  "/placeholder/9.svg",
+  "/placeholder/10.svg"
+]
+
+export const create = mutation({
+  args: {
+    orgId: v.string(),
+    title: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+  }
+})
