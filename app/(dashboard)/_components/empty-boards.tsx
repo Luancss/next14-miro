8acc1bf9498da02fdcd3f50bce71b594f1ae339d
Luancss <ventobo123@gmail.com>
2024-02-26 18:45:24 -0300

    Add toast

--- app/(dashboard)/_components/empty-boards.tsx
+++ app/(dashboard)/_components/empty-boards.tsx
@@ -1,41 +1,40 @@
 "use client";
 
 import Image from "next/image";
-import { useMutation } from "convex/react";
 
 import { api } from "@/convex/_generated/api";
 import { Button } from "@/components/ui/button";
 import { useOrganization } from "@clerk/nextjs";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { toast } from "sonner";
 
 export const EmptyBoards = () => {
-  const {organization} = useOrganization();
-  const create = useMutation(api.board.create);
+  const { organization } = useOrganization();
+  const { mutate, pending } = useApiMutation(api.board.create);
 
-  const onClick =  () => {
-    if (!organization) return null
-
-    create({
+  const onClick = () => {
+    if (!organization) return null;
+    mutate({
       orgId: organization.id,
-      title: "Untitled"
+      title: "Untitled",
     })
-  }
+      .then((id) => {
+        toast.success("Board created");
+      })
+      .catch((error) => {
+        toast.error("Failed to create board");
+      });
+  };
 
   return (
     <div className="h-full flex flex-col items-center justify-center">
-      <Image
-        src="/note.svg"
-        height={110}
-        width={110}
-        alt="Empty"
-      />
-      <h2 className="text-2xl font-semibold mt-6">
-        Create your first board!
-      </h2>
+      <Image src="/note.svg" height={110} width={110} alt="Empty" />
+      <h2 className="text-2xl font-semibold mt-6">Create your first board!</h2>
       <p className="text-muted-foreground textg-sm mt-2">
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button onClick={onClick} size="lg">
+        <Button disabled={pending} onClick={onClick} size="lg">
           Create board
         </Button>
       </div>
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+
+import { Toaster } from "@/components/ui/sonner";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -19,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <ConvexClientProvider>
+          <Toaster/>
         {children}
         </ConvexClientProvider>
       </body>
