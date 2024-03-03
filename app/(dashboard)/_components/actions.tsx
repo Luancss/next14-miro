ed267269acc9a0fb50b151bcbb91ec706d883598
Luancss <ventobo123@gmail.com>
2024-03-03 03:09:35 -0300

    Delete funtionality board success

--- app/(dashboard)/_components/actions.tsx
+++ app/(dashboard)/_components/actions.tsx
@@ -9,8 +9,10 @@ import {
   DropdownMenuItem,
   DropdownMenuSeparator,
 } from "@/components/ui/dropdown-menu";
-import { Link2 } from "lucide-react";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { Link2, Trash2 } from "lucide-react";
 import { toast } from "sonner";
+import { api } from "@/convex/_generated/api";
 
 interface ActionProps {
   children: React.ReactNode;
@@ -27,36 +29,46 @@ export const Actions = ({
   id,
   title,
 }: ActionProps) => {
+  const { mutate, pending } = useApiMutation(api.board.remove);
+
   const onCopyLink = () => {
-    navigator.clipboard.writeText(
-      `${window.location.origin}/board/${id}`
-      )
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
       .then(() => {
         toast.success("Link copied!");
       })
       .catch(() => {
         toast.error("Failed to copy link");
+      });
+  };
+
+  const onDelete = () => {
+    mutate({ id })
+      .then(() => {
+        toast.success("Board deleted");
       })
-  }
-  
+      .catch(() => {
+        toast.error("Failed to delete board");
+      });
+  };
+
   return (
     <DropdownMenu>
-      <DropdownMenuTrigger asChild>
-        {children}
-      </DropdownMenuTrigger>
+      <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
       <DropdownMenuContent
         onClick={(e) => e.stopPropagation()}
         side={side}
         sideOffset={sideOffset}
         className="w-60"
-        >
-          <DropdownMenuItem
-          onClick={onCopyLink}
-            className="p-3 cursor-pointer"
-          >
-            <Link2 className="h-4 w-4 mr-2"/>
-            Copy board link
-          </DropdownMenuItem>
+      >
+        <DropdownMenuItem onClick={onCopyLink} className="p-3 cursor-pointer">
+          <Link2 className="h-4 w-4 mr-2" />
+          Copy board link
+        </DropdownMenuItem>
+        <DropdownMenuItem onClick={onDelete} className="p-3 cursor-pointer">
+          <Trash2 className="h-4 w-4 mr-2" />
+          Delete
+        </DropdownMenuItem>
       </DropdownMenuContent>
     </DropdownMenu>
   );
