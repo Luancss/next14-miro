47562ab8c4a1f39b82a3791cbbb645a1272cda4b
Luancss <ventobo123@gmail.com>
2024-03-03 03:04:12 -0300

    Implementing board removal functionality

--- convex/board.ts
+++ convex/board.ts
@@ -42,3 +42,16 @@ export const create = mutation({
     return board;
   }
 })
+
+export const remove = mutation({
+  args: {id: v.id("boards")},
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    await ctx.db.delete(args.id);
+  }
+})
