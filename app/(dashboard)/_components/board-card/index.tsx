2b32342e96781a50d0a2f3c494952152bea27d0b
Luancss <ventobo123@gmail.com>
2024-03-04 15:44:20 -0300

    Adding board card funtionality favorite

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -9,7 +9,10 @@ import { MoreHorizontal } from "lucide-react";
 import { Footer } from "./footer";
 
 import { Skeleton } from "@/components/ui/skeleton";
-import { Actions } from "../../../../components/actions";
+import { Actions } from "@/components/actions";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 interface BoardCardProps {
   id: string;
@@ -37,19 +40,33 @@ export const BoardCard = ({
   const authorLabel = userId === authorId ? "You" : authorName;
   const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
 
+  const { mutate: onFavorite, pending: pendingFavorite } = useApiMutation(
+    api.board.favorite
+  );
+  const { mutate: onUnFavorite, pending: pendingUnFavorite } = useApiMutation(
+    api.board.favorite
+  );
+
+  const toggleFavorite = () => {
+    if (isFavorite) {
+      onUnFavorite({ id }).catch(() => toast.error("Failed to unfavorite"));
+    } else {
+      onFavorite({ id, orgId }).catch(() => toast.error("Failed to favorite"));
+    }
+  };
+
   return (
     <Link href={`/board/${id}`}>
       <div className="group aspect-[100/127] border rounded-lg flex flex-col justify-between overflow-hidden">
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fit" />
           <Overlay />
-          <Actions 
-            id={id}
-            title={title}
-            side="right"
-          >
-            <button title="More" className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition px-3 py-2 outline-none">
-              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity "/>
+          <Actions id={id} title={title} side="right">
+            <button
+              title="More"
+              className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition px-3 py-2 outline-none"
+            >
+              <MoreHorizontal className="text-white opacity-75 hover:opacity-100 transition-opacity " />
             </button>
           </Actions>
         </div>
@@ -58,8 +75,8 @@ export const BoardCard = ({
           title={title}
           authorLabel={authorLabel}
           createdAtLabel={createdAtLabel}
-          onClick={() => {}}
-          disabled={false}
+          onClick={toggleFavorite}
+          disabled={pendingFavorite || pendingUnFavorite}
         />
       </div>
     </Link>
