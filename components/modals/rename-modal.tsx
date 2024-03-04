31fa2cbfc00f7b5e93171d96a47f0a06e9917535
Luancss <ventobo123@gmail.com>
2024-03-04 02:43:46 -0300

    Estructure component

---
+++ components/modals/rename-modal.tsx
@@ -0,0 +1,36 @@
+"use client"
+
+import {
+  Dialog,
+  DialogContent,
+  DialogTrigger,
+  DialogHeader,
+  DialogFooter,
+  DialogTitle,
+  DialogDescription,
+  DialogClose
+} from "@/components/ui/dialog"
+import { useRenameModal } from "@/store/use-rename-modal"
+
+export const RenameModal = () => {
+  const {
+    isOpen,
+    onClose,
+    initialValues
+  } = useRenameModal()
+
+  return (
+    <Dialog open={isOpen} onOpenChange={onClose}>
+      <DialogContent>
+        <DialogHeader>
+          <DialogTitle>
+            Edit board title
+          </DialogTitle>
+        </DialogHeader>
+        <DialogDescription>
+          Enter a new title for this board
+        </DialogDescription>
+      </DialogContent>
+    </Dialog>
+  )
+}
