714fbc2a0846ef485bb06a71adb414b95b4c99d5
Luancss <ventobo123@gmail.com>
2024-03-04 03:11:05 -0300

    Implements funtionality on submit rename

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { FormEventHandler, useEffect, useState } from "react";
 
 import {
   Dialog,
@@ -15,8 +15,16 @@ import {
 import { useRenameModal } from "@/store/use-rename-modal";
 import { Input } from "@/components//ui/input";
 import { Button } from "@/components/ui/button";
+import { useApiMutation } from "@/hooks/use-api-mutation";
+import { api } from "@/convex/_generated/api";
+import { toast } from "sonner";
 
 export const RenameModal = () => {
+  const {
+    mutate,
+    pending
+  } = useApiMutation(api.board.update);
+  
   const { isOpen, onClose, initialValues } = useRenameModal();
 
   const [title, setTitle] = useState(initialValues.title);
@@ -25,7 +33,23 @@ export const RenameModal = () => {
     setTitle(initialValues.title);
   }, [initialValues.title]);
 
-  const onSubmit = () => {};
+  const onSubmit: FormEventHandler<HTMLFormElement> = (
+    e,
+  ) => {
+    e.preventDefault();
+
+    mutate({
+      id: initialValues.id,
+      title,
+    })
+      .then(() => {
+        toast.success("Board renamed");
+        onClose();
+      })
+      .catch(() => {
+        toast.error("Failed to rename board");
+      })
+  };
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -36,7 +60,7 @@ export const RenameModal = () => {
         <DialogDescription>Enter a new title for this board</DialogDescription>
         <form onSubmit={onSubmit} className="space-y-4">
           <Input
-            disabled={false}
+            disabled={pending}
             required
             maxLength={60}
             value={title}
@@ -49,7 +73,7 @@ export const RenameModal = () => {
                 Cancel
                 </Button>
             </DialogClose>
-            <Button disabled={false} type="submit">
+            <Button disabled={pending} type="submit">
               Save
             </Button>
           </DialogFooter>
