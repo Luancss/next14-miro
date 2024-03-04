fc923987b9991d980b5884cc2ee1b6f301011d92
Luancss <ventobo123@gmail.com>
2024-03-04 15:35:40 -0300

    Delete favorites on database

--- convex/board.ts
+++ convex/board.ts
@@ -126,6 +126,45 @@ export const favorite = mutation({
       orgId: args.orgId,
     });
 
-    return board
+    return board;
+  },
+});
+
+export const unFavorite = mutation({
+  args: {
+    id: v.id("boards"),
+    orgId: v.string(),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) {
+      throw new Error("Unauthorized");
+    }
+
+    const board = await ctx.db.get(args.id);
+
+    if (!board) {
+      throw new Error("Board not found");
+    }
+
+    const userId = identity.subject;
+
+    const existingFavorite = await ctx.db
+      .query("userFavorites")
+      .withIndex("by_user_board", (q) =>
+        q
+        .eq("userId", userId)
+        .eq("boardId", board._id)
+    )
+    .unique();
+
+    if (!existingFavorite) {
+      throw new Error("Favorited board not found");
+    }
+
+    await ctx.db.delete(existingFavorite._id);
+
+    return board;
   },
 });
