9acda92a1930afe8e8a59485e16c5915433b5f48
Luancss <ventobo123@gmail.com>
2024-03-06 03:28:28 -0300

    Room authentication complete

--- app/board/[boardId]/_components/canvas.tsx
+++ app/board/[boardId]/_components/canvas.tsx
@@ -4,6 +4,8 @@ import { Info } from "./info";
 import { Participants } from "./participants";
 import { Toolbar } from "./toolbar";
 
+import { useSelf } from "@/liveblocks.config";
+
 interface CanvasProps {
   boardId: string;
 }
@@ -11,6 +13,10 @@ interface CanvasProps {
 export const Canvas = ({
   boardId
 }: CanvasProps) => {
+  const info = useSelf((me) => me.info);
+
+  console.log(info);
+
   return( 
   <main
     className="w-full h-full relative bg-neutral-100 touch-none"
