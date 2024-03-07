005b6ed2929daa166070395248f6ca92617aaf69
Luancss <ventobo123@gmail.com>
2024-03-07 17:57:26 -0300

    Create type of canvas

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -41,8 +41,8 @@ export const Toolbar = ({
         <ToolButton
           label="Text"
           icon={Type}
-          onClick={() => {}}
-          isActive={false}
+          onClick={() => setCanvasState({ mode: CanvasMode.Inserting })}
+          isActive={canvasState.mode === CanvasMode.Inserting}
         />
         <ToolButton
           label="Sticky note"
--- types/canvas.ts
+++ types/canvas.ts
@@ -1,3 +1,73 @@
+export type Color = {
+  r: number;
+  g: number;
+  b: number
+}
+
+export type Camera = {
+  x: number;
+  y: number;
+}
+
+export enum LayerType {
+  Rectangle,
+  Ellipse,
+  Path,
+  Text,
+  Note
+}
+
+export type RetangleLayer = {
+  type: LayerType.Rectangle;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: Color;
+  value?: string;
+}
+
+export type EllipseLayer = {
+  type: LayerType.Ellipse;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: Color;
+  value?: string;
+}
+
+export type PathLayer = {
+  type: LayerType.Ellipse;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: Color;
+  points: number[][];
+  value?: string;
+}
+
+export type TextLayer = {
+  type: LayerType.Text;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: Color;
+  value?: string;
+}
+
+export type NoteLayer = {
+  type: LayerType.Note;
+  x: number;
+  y: number;
+  width: number;
+  height: number;
+  fill: Color;
+  value?: string;
+}
+
 export type CanvasState = 
   | {
      mode: CanvasMode.None;
