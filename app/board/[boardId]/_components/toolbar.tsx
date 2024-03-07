19d545de6e228cb4f32b4844ca35c5fbc99e4379
Luancss <ventobo123@gmail.com>
2024-03-07 15:32:35 -0300

    Add icons on toolbar

--- app/board/[boardId]/_components/toolbar.tsx
+++ app/board/[boardId]/_components/toolbar.tsx
@@ -1,4 +1,11 @@
-import { MousePointer } from "lucide-react";
+import {
+  Circle,
+  MousePointer,
+  Pencil,
+  Square,
+  StickyNote,
+  Type
+} from "lucide-react";
 import { ToolButton } from "./tool-button";
 
 export const Toolbar = () => {
@@ -9,6 +16,37 @@ export const Toolbar = () => {
          label="Select"
           icon={MousePointer}
           onClick={() => { }}
+          isActive={false}
+          />
+          <ToolButton
+         label="Text"
+          icon={Type}
+          onClick={() => { }}
+          isActive={false}
+          />
+           <ToolButton
+         label="Sticky note"
+          icon={StickyNote}
+          onClick={() => { }}
+          isActive={false}
+          />
+           <ToolButton
+         label="Rectangle"
+          icon={Square}
+          onClick={() => { }}
+          isActive={false}
+          />
+          <ToolButton
+         label="Ellipse"
+          icon={Circle}
+          onClick={() => { }}
+          isActive={false}
+          />
+          <ToolButton
+         label="Pen"
+          icon={Pencil}
+          onClick={() => { }}
+          isActive={false}
           />
       </div>
       <div className="bg-white rounded-md p-1.5 flex flex-col items-center shadow-md">
