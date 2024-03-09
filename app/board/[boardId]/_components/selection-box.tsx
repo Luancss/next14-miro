630f22f682074bfca7a41fcc852572ea5c328c3c
Luancss <ventobo123@gmail.com>
2024-03-09 17:13:41 -0300

    Add bounding box visual representation

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,27 +1,46 @@
 "use client";
 
+import { useSelectionBounds } from "@/hooks/use-selection-bounds";
 import { useSelf, useStorage } from "@/liveblocks.config";
 import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
 
 interface SelectionBoxProps {
   onResizeHandlePointerDown: (corner: Side, initialBounds: XYWH) => void;
-};
+}
 
 const HANDLE_WIDTH = 8;
 
-export const SelectionBox = memo(({ onResizeHandlePointerDown }: SelectionBoxProps) => {
-  const soleLayerId = useSelf((me) => 
-    me.presence.selection.length === 1 ? me.presence.selection[0] : null
-  );
+export const SelectionBox = memo(
+  ({ onResizeHandlePointerDown }: SelectionBoxProps) => {
+    const soleLayerId = useSelf((me) =>
+      me.presence.selection.length === 1 ? me.presence.selection[0] : null
+    );
 
-  const isShowingHandles = useStorage((root) => 
-    soleLayerId && root.layers.get(soleLayerId)?.type === LayerType.Path
-  )
+    const isShowingHandles = useStorage(
+      (root) =>
+        soleLayerId && root.layers.get(soleLayerId)?.type === LayerType.Path
+    );
 
-  return (
-    <div>
+    const bounds = useSelectionBounds();
 
-    </div>
-  );
-})
+    if (!bounds) {
+      return null
+    }
+
+    return (
+      <>
+      <rect
+        className="fill-transparen stroke-blue-500 stroke-1 pointer-events-none"
+        style={{
+          transform: `translate(${bounds.x}px, ${bounds.y}px)`,
+        }}
+        x={0}
+        y={0}
+        width={bounds.width}
+        height={bounds.height}
+      />
+      </>
+    )
+  }
+);
