0d7c380a3eb2d374955bf20c5ade6d640246a320
Luancss <ventobo123@gmail.com>
2024-03-09 16:58:59 -0300

    Implement SelectionBox component with resize handles

--- app/board/[boardId]/_components/selection-box.tsx
+++ app/board/[boardId]/_components/selection-box.tsx
@@ -1,18 +1,27 @@
 "use client";
 
+import { useSelf, useStorage } from "@/liveblocks.config";
+import { LayerType, Side, XYWH } from "@/types/canvas";
 import { memo } from "react";
 
 interface SelectionBoxProps {
-  onResizeHandlePointerDown: () => void;
+  onResizeHandlePointerDown: (corner: Side, initialBounds: XYWH) => void;
 };
 
+const HANDLE_WIDTH = 8;
+
 export const SelectionBox = memo(({ onResizeHandlePointerDown }: SelectionBoxProps) => {
+  const soleLayerId = useSelf((me) => 
+    me.presence.selection.length === 1 ? me.presence.selection[0] : null
+  );
+
+  const isShowingHandles = useStorage((root) => 
+    soleLayerId && root.layers.get(soleLayerId)?.type === LayerType.Path
+  )
+
   return (
-    <div
-      className="absolute top-0 left-0 w-full h-full"
-      onPointerDown={onResizeHandlePointerDown}
-    >
-      
+    <div>
+
     </div>
   );
 })
