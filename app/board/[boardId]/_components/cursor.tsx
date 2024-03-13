db84982dcf01dbfa4a1ebf3ad98251589b46df72
Luancss <ventobo123@gmail.com>
2024-03-13 02:01:27 -0300

    Fixing error

--- app/board/[boardId]/_components/cursor.tsx
+++ app/board/[boardId]/_components/cursor.tsx
@@ -3,37 +3,38 @@
 import { memo } from "react";
 import { MousePointer2 } from "lucide-react";
 
-import { connectionIdToColor } from "@/lib/utils";
 import { useOther } from "@/liveblocks.config";
+import { connectionIdToColor } from "@/lib/utils";
 
 interface CursorProps {
   connectionId: number;
-}
+};
 
-export const Cursor = memo(({ connectionId }: CursorProps) => {
+export const Cursor = memo(({
+  connectionId,
+}: CursorProps) => {
   const info = useOther(connectionId, (user) => user?.info);
-  const cursor = useOther(connectionId, (user) => user.presence.cursor);
+  const cursor = useOther(connectionId, (user) => user.presence.cursor)
 
   const name = info?.name || "Teammate";
-
 
   if (!cursor) {
     return null;
   }
 
-  const {x, y} = cursor;
+  const { x, y } = cursor;
 
   return (
     <foreignObject
       style={{
-        transform: `translateX(${x}px, translateY(${y}px`,
+        transform: `translateX(${x}px) translateY(${y}px)`
       }}
       height={50}
       width={name.length * 10 + 24}
       className="relative drop-shadow-md"
     >
       <MousePointer2
-        className="w-5 h-5"
+        className="h-5 w-5"
         style={{
           fill: connectionIdToColor(connectionId),
           color: connectionIdToColor(connectionId),
@@ -41,12 +42,12 @@ export const Cursor = memo(({ connectionId }: CursorProps) => {
       />
       <div
         className="absolute left-5 px-1.5 py-0.5 rounded-md text-xs text-white font-semibold"
-        style={{
-          backgroundColor: connectionIdToColor(connectionId),
-        }}
+        style={{ backgroundColor: connectionIdToColor(connectionId) }}
       >
         {name}
       </div>
     </foreignObject>
-  );
+  )
 });
+
+Cursor.displayName = "Cursor";
--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,8 @@ import "./globals.css";
 import { Toaster } from "@/components/ui/sonner";
 import { ConvexClientProvider } from "@/providers/convex-client-provider";
 import { ModalProvider } from "@/providers/modal-provider";
+import { Suspense } from "react";
+import { Loading } from "@/components/auth/loading";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -21,11 +23,13 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <ConvexClientProvider>
-          <Toaster/>
-          <ModalProvider/>
-        {children}
-        </ConvexClientProvider>
+        <Suspense fallback={<Loading/>}>
+          <ConvexClientProvider>
+            <Toaster />
+            <ModalProvider />
+            {children}
+          </ConvexClientProvider>
+        </Suspense>
       </body>
     </html>
   );
